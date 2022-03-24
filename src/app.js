038394ab40c8b09514a620474ecc6340bd0c6a16
animekkk <animekgaming@gmail.com>
2022-03-24 16:39:02 +0100

    removing chat endpoints

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import rateLimit from 'express-rate-limit';
 import cors from 'cors';
 import SimpleNodeLogger from 'simple-node-logger';
-import { userRouter, groupsRouter, honorsRouter, videosRouter, videoRouter, chattersRouter, xayoRouter } from './routes'
+import { groupsRouter, honorsRouter, videosRouter, videoRouter, xayoRouter } from './routes'
 import { measure } from './metrics';
 
 export const app = express();
@@ -12,16 +12,6 @@ export const log = SimpleNodeLogger.createSimpleLogger({
     timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
 });
 
-const chatLimiter = rateLimit({
-    windowMs: 5000,
-    max: 1,
-    message: { error: 'Too many requests in 5 second.' }
-});
-const chattersLimiter = rateLimit({
-    windowMs: 15000,
-    max: 3,
-    message: { error: 'Too many requests in 15 seconds.' }
-});
 const globalLimiter = rateLimit({
     windowMs: 1000,
     max: 3,
@@ -37,8 +27,6 @@ app.use(express.static('public'));
 app.set('json spaces', 2);
 app.set('trust proxy', 1);
 
-app.use('/chat', userRouter, chatLimiter);
-app.use('/chatters', chattersRouter, chattersLimiter);
 app.use('/groupBadges', groupsRouter, globalLimiter);
 app.use('/honors', honorsRouter, globalLimiter);
 app.use('/videos', videosRouter, globalLimiter);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
-export { userRouter } from './chat/user';
 export { groupsRouter } from './groups/';
 export { honorsRouter } from './honors/';
 export { videosRouter } from './videos/videos';
 export { videoRouter } from './videos/video'
-export { chattersRouter } from './chat/chatters';
 export { xayoRouter } from './user/xayo'
